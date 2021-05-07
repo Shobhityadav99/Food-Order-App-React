e5a1466a4cde6a3cce71d1b208b2f91a6c5b5bb8
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-07 20:08:45 +0530

    Header done

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
+import classes from "./Header.module.css";
+import MealsImage from '../../assets/meals.jpg';
 
 const Header = props => {
     return <React.Fragment>
-        <header>
+        <header className={classes.header}>
             <h1>React Meals</h1>
             <button>Cart</button>
         </header>
-        <div>
-            
+        <div className={classes["main-image"]}>
+            <img src={MealsImage} alt="Khaana hi khaana BC!"></img>
         </div>
     </React.Fragment>
 }

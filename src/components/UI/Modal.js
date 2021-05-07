9554da4fb1581c33dd05bd3314a1a1e6415805de
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-07 22:14:01 +0530

    Modal done

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,5 +1,21 @@
+import { Fragment } from "react";
+import classes from "./Modal.module.css";
+
+const BackDrop = props => {
+    return <div className={classes.backdrop} />
+}
+
+const ModalOverlay = props => {
+    return <div className={classes.modal} >
+        <div className={classes.content}>{props.children}</div>
+    </div>
+}
+
 const Modal = props =>{
-    
+    return <Fragment>
+        <BackDrop />
+        <ModalOverlay />
+    </Fragment>
 }
 
 export default Modal;

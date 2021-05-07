acbe1f0b9b88c047b4e62f8a65ac65f2d6de2f33
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-07 20:17:41 +0530

    Header done

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Header.module.css";
 import MealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = props => {
     return <React.Fragment>
         <header className={classes.header}>
             <h1>React Meals</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes["main-image"]}>
             <img src={MealsImage} alt="Khaana hi khaana BC!"></img>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,16 @@
-const HeadercartButton = props => {
-    return <button>
-        <span></span>
-        <span></span>
-        <span></span>
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon"
+
+const HeaderCartButton = props => {
+    return <button className={classes.button}>
+        <span className={classes.icon}>
+            <CartIcon />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>
+            3
+        </span>
     </button>
 }
 
-export default HeadercartButton;
+export default HeaderCartButton;

99d898bdc52cd61ce225b5ae0f1bd9a2bc377f9f
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-07 22:04:53 +0530

    Cart in progress

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,13 @@
-const Cart = props => {
+import classes from "./Cart.module.css";
 
+const Cart = props => {
+    const cartItems = <ul className={classes['class-item']}>{[{id: 'c1',name: 'pijja',amount: '2',price: 12.69}]
+    .map(item => <li>{item.name}</li>)}</ul>
+    return <div>
+        {cartItems}
+        <div></div>
+        <div></div>
+    </div>
 }
 
 export default Cart;

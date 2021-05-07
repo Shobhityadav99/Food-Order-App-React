9cb480f3d7583bec0451500a240a51124dfe7920
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-07 22:17:02 +0530

    Modal done

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -12,12 +12,12 @@ const ModalOverlay = props => {
     </div>
 }
 
-
+const portalElement = document.getElementById('overlays');
 
 const Modal = props =>{
     return <Fragment>
-        {ReactDOM.createPortal(<BackDrop/>)}
-        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>)}
+        {ReactDOM.createPortal(<BackDrop/>,portalElement)}
+        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
     </Fragment>
 }
 

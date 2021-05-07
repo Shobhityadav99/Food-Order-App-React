eb5b2a765db741d9685f11e3a1af1b6a9d0ed37f
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-07 20:51:00 +0530

    Meals showing good

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,12 @@
+import classes from "*.module.css";
+
 const MealItem = props => {
     const price=`$${props.price.toFixed(2)}`;
-    return <li>
+    return <li className={classes.meal}>
         <div>
             <h3>{props.name}</h3>
-            <div>{props.description}</div>
-            <div>{price}</div>
+            <div className={classes.description}>{props.description}</div>
+            <div classNam={classes.price}>{price}</div>
         </div>
         <div></div>
     </li>

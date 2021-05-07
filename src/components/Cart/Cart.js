39992d23d87fe0a77c245acd1e246835c9e5d94c
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-07 22:31:26 +0530

    Modal done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,10 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = props => {
     const cartItems = <ul className={classes['class-item']}>{[{id: 'c1',name: 'pijja',amount: '2',price: 12.69}]
     .map(item => <li>{item.name}</li>)}</ul>
-    return <div>
+    return <Modal>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
@@ -13,7 +14,7 @@ const Cart = props => {
             <button className={classes['button--alt']}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
-    </div>
+    </Modal>
 }
 
 export default Cart;

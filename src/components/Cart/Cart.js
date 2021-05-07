d2de0c28862d2d940804a08b2305735efd4edf1e
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-07 22:44:24 +0530

    Close working

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,14 +4,14 @@ import classes from "./Cart.module.css";
 const Cart = props => {
     const cartItems = <ul className={classes['class-item']}>{[{id: 'c1',name: 'pijja',amount: '2',price: 12.69}]
     .map(item => <li>{item.name}</li>)}</ul>
-    return <Modal>
+    return <Modal onClose={props.onClose}>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
             <span>69.69</span>
         </div>
         <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
+            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
     </Modal>

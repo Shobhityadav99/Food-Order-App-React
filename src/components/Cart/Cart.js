ba7d237e1415f2a91c8141497888851ac51f2bb4
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-08 12:11:47 +0530

    Adding item done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,14 +2,24 @@ import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = props => {
     const cartCtx = useContext(CartContext);
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
+    const cartItemRemoveHandler = id => {};
+    const cartItemAddHandler = item => {};
     const cartItems = <ul className={classes['class-item']}>
         {cartCtx.items
-            .map(item => <li>{item.name}</li>)}</ul>
+            .map(item => <CartItem 
+                key={item.id} 
+                name={item.name} 
+                price={item.price}
+                amount={item.amount} 
+                onRemove={cartItemRemoveHandler.bind(null,item.id)} 
+                onAdd={cartItemAddHandler.bind(null,item)} />
+                )}</ul>
     return <Modal onClose={props.onClose}>
         {cartItems}
         <div className={classes.total}>

65e0332701f77b78990de97ae6f00747e06b9308
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-08 11:25:54 +0530

    Ref in use

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,16 +1,30 @@
+import { useRef } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemform.module.css";
 
 const MealItemForm = props => {
-    return <form className={classes.form}>
-        <Input label="Amount" input= {{
-            id: 'amount_' + props.id,
-            type: 'number',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '1'
-        }}/>
+    const amountInputRef = useRef();
+    const submitHandler = event => {
+        event.preventDefault();
+        const enteredAmont = amountInputRef.current.value;
+        const enteredAmontNumber = +enteredAmont;
+
+        if (enteredAmont.trim().length === 0 || enteredAmontNumber < 1 || enteredAmontNumber > 5) {
+            return;
+        }
+    }
+    return <form className={classes.form} onSubmit={submitHandler}>
+        <Input
+            ref={amountInputRef}
+            label="Amount"
+            input={{
+                id: 'amount_' + props.id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }} />
         <button>+ Add</button>
     </form>
 }
